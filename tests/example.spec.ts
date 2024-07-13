d0429974e952adf9ebda1a8a2336cead16b8df9b
sanjeev.kumar1 <renukasharma25sept@gmail.com>
2024-07-14 01:24:59 +0530

    test5

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Adjust path to match where the artifact is downloaded
-const paymentsFilePath = path.join(__dirname, '../../../async-ci-automation/data/payments.json');
+const paymentsFilePath = path.join(__dirname, '../../../../async-ci-automation/data/payments.json');
 
 // Read payment data from file
 const payments = JSON.parse(fs.readFileSync(paymentsFilePath, 'utf8'));
