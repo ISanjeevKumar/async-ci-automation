53ba04dfce277f0adbde825dc0b37fcb8152f451
sanjeev.kumar1 <renukasharma25sept@gmail.com>
2024-07-14 01:23:11 +0530

    test2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from '@playwright/test';
 const fs = require('fs');
 const path = require('path');
-import * as payments from "../data/payments.json"
+
+// Adjust path to match where the artifact is downloaded
+const paymentsFilePath = path.join(__dirname, '../../../async-ci-automation/data/payments.json');
+
+// Read payment data from file
+const payments = JSON.parse(fs.readFileSync(paymentsFilePath, 'utf8'));
 
 test.describe('Journey 2 E2E Tests', () => {
   payments.forEach(payment => {
